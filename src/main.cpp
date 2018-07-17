05bdfeb75476405402fbce91836d25afd74cda3c
zxliang <zxliang@umn.edu>
2018-07-16 20:16:01 -0500

    corrected problem 1

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,19 @@ using namespace std;
 class Solution {
 public:
   int findMatrixNumber(int x, int y, int edge=4) {
-    vector<int> coordinates = helper(edge, vector<int>{x, y});
-    cout << coordinates[0] << " " << coordinates[1] << endl;
-    return coordinates[0]*edge + coordinates[1];
+    return helper(edge, vector<int>{x, y});
   }
 
-  vector<int> helper(int edge, vector<int> cur) {
-    if (edge==1) return vector<int>{0, 0};
+  int helper(int edge, vector<int> cur) {
+    if (edge==1) return 0;
     edge/=2;
-    vector<int> next = helper(edge, vector<int>{cur[0]%edge, cur[1]%edge});
+    int next = helper(edge, vector<int>{cur[0]%edge, cur[1]%edge});
     if (cur[0]<edge && cur[1]<edge)
-      next[1] += edge/2;
+      next += edge*edge;
     else if (cur[0]<edge && cur[1]>=edge)
-      next[1] += edge/2;
+      next += edge*edge*2;
     else if (cur[0]>=edge && cur[1]>=edge)
-      next[0] += edge/2, next[1] += edge/2; 
+      next += edge*edge*3; 
     return next;
   }
 };
@@ -32,7 +30,9 @@ int main(int argc, char* argv[])
   cout << "Starting function tests! \n" << endl;
 
   Solution solu;
-  cout << "Matrix value at (" << 3 << ", " << 2 << "): " << solu.findMatrixNumber(3, 2) << endl;
+  for (int i=0; i<4; i++) 
+    for (int j=0; j<4; j++) 
+      cout << "Matrix value at (" << i << ", " << j << "): " << solu.findMatrixNumber(i, j) << endl;
 
   return 0;
 }
